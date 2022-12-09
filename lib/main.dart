cc3af9c80a4636476c6f29d2b04346ebaa399a82
kithya <kithya.um@z1fsc.com>
2022-12-09 11:25:45 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+import 'package:dashboard_cic/widget/custom_box.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
   //testing git
 }
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const DashboardScreen(
+          title: "Dashboard",
+        );
+      },
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,27 +28,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routerConfig: _router,
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(480, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,10 +57,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DashboardScreenState extends State<DashboardScreen> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -70,47 +82,106 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.blue[50],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return constraints.maxWidth > 800
+              ? const DesktopAndTablet()
+              : const Mobile();
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+    // This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
+
+class Mobile extends StatelessWidget {
+  const Mobile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: const [
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 20),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Live Dashboard",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Padding(
+              padding:
+                  EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+              child: CustomBox(
+                iconData: CupertinoIcons.ticket_fill,
+                subTitle: "UT",
+                title: "1,000,000",
+              )),
+          Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: CustomBox()),
+        ],
+      ),
+    );
+  }
+}
+
+class DesktopAndTablet extends StatelessWidget {
+  const DesktopAndTablet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 20, left: 20),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Live Dashboard",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            children: const [
+              Expanded(
+                  flex: 15,
+                  child: CustomBox(
+                    iconData: CupertinoIcons.ticket_fill,
+                    subTitle: "UT",
+                    title: "1,000,000",
+                  )),
+              Expanded(
+                flex: 1,
+                child: SizedBox(
+                  width: double.infinity,
+                ),
+              ),
+              Expanded(
+                flex: 15,
+                child: CustomBox(),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
